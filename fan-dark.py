4b2dacec912504b52de6911ca112599b747245a5
Azure <63315512+azure-possum@users.noreply.github.com>
2022-03-02 10:38:46 -0300

    Indentation issues corrected

--- fan-dark.py
+++ fan-dark.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
 import tkinter as tk
-import os
 import subprocess
+import pystray
 from tkinter import *
 from time import sleep
 from threading import Thread
+from pystray import MenuItem as item
+from PIL import Image
 
 
-#Get system info
-import subprocess as sub
+def exit_action(icon):
+    icon.visible = False
+    icon.stop()
+
+
+def show_window(icon):
+    icon.stop()
+    root.after(0, root.deiconify())
+
+
+def quit_window(icon):
+    icon.stop()
+    root.destroy()
+
+
+def hide_window():
+    root.withdraw()
+    image = Image.open("/opt/fancontrol/Resources/icon.png")
+    menu = (
+        item('Quit', lambda: exit_action(icon)),
+        item('Show', show_window, default=True)
+        )
+    icon = pystray.Icon("name", image, "Thinkfan Control", menu)
+    icon.run()
+
 
 def set_speed(speed=None):
     """
@@ -20,7 +45,8 @@ def set_speed(speed=None):
         'echo level {0} | sudo tee "/proc/acpi/ibm/fan"'.format(speed),
         shell=True
     ).decode()
-    
+
+
 def get_info():
     info_lines = subprocess.check_output("sensors").decode("utf-8").split("\n")
     result = []
@@ -32,48 +58,37 @@ def get_info():
 
         if "fan" in i:
             result.append("Fan : " + i.split(":")[-1].strip())
-            count +=1
-
-        if "Level" in i:
-            result.append(("Level : %r" % speed) + i.split(":")[1].strip())
+            count += 1
     return result
-
-def printLevel():
-    print("set level to %r" % speed)
-
 
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
-        root.tk_setPalette(background='#383838')
+        root.configure(background='black')
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.parent.minsize(width=100, height=100)
 
-        main_label = tk.Label(parent, text="",)
+        main_label = tk.Label(parent, text="", bg="#000000", fg="white")
         main_label.grid(row=0, column=0)
 
         row1 = tk.Frame()
         row1.grid()
 
         for i in range(8):
-            tk.Button(row1, text=str(i), command=lambda x=i: set_speed(x)).grid(
+            tk.Button(row1, text=str(i), highlightbackground="#1A1C1A", bg="#000000", fg="white", highlightcolor="#1A1C1A", highlightthickness=3, bd=0, activebackground="#e60012", activeforeground="white", command=lambda x=i: set_speed(x)).grid(
                 row=0, column=i + 1
             )
 
-
         row2 = tk.Frame()
         row2.grid()
-        
 
-        tk.Button(row2, text="Auto", command=lambda: set_speed("auto")).grid(
+        tk.Button(row2, text="Auto", highlightbackground="#1A1C1A", bg="#000000", fg="white", highlightcolor="#1A1C1A", highlightthickness=3, bd=0, activebackground="#e60012", activeforeground="white", command=lambda: set_speed("auto")).grid(
             row=0, column=0
         )
-        tk.Button(row2, text="Full", command=lambda: set_speed("full-speed")).grid(
+        tk.Button(row2, text="Full", highlightbackground="#1A1C1A", bg="#000000", fg="white", highlightcolor="#1A1C1A", highlightthickness=3, bd=0, activebackground="#e60012", activeforeground="white", command=lambda: set_speed("full-speed")).grid(
             row=0, column=1
         )
-        
-
 
         def display_loop():
             while True:
@@ -86,9 +101,9 @@ class MainApplication(tk.Frame):
 if __name__ == "__main__":
 
     root = tk.Tk()
-    img = tk.Image("photo", file='/home/azure/thinkfan-control-gui-master/icon.png')
-    root.tk.call('wm','iconphoto',root._w,img)
+    img = tk.Image("photo", file='/opt/fancontrol/Resources/icon.png')
+    root.protocol('WM_DELETE_WINDOW', hide_window)
+    root.tk.call('wm', 'iconphoto', root._w, img)
     root.title("Thinkfan Control")
     MainApplication(root).grid()
     root.mainloop()
-    
