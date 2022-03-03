67fa9756d08b4a0ad637306b69fbb8863dbb9ac5
Azure <63315512+azure-possum@users.noreply.github.com>
2022-03-02 21:02:11 -0300

    using gksu instead of a background terminal

--- install.sh
+++ install.sh
@@ -9,15 +9,7 @@ case "$mode" in
     2) cp src/fan-dark.py $install_folder/src/fan.py ;;
     *) echo "Input not valid" ;;
 esac
-echo "Enter 1 for ElementaryOS, 2 for Gnome, 3 for KDE or 4 for LXDE"
-read mode
-case "$mode" in
-    1) cp Resources/thinkfan-pantheon.desktop /usr/share/applications/thinkfan.desktop ;;
-    2) cp Resources/thinkfan-gnome.desktop /usr/share/applications/thinkfan.desktop ;;
-    3) cp Resources/thinkfan-kde.desktop /usr/share/applications/thinkfan.desktop ;;
-    4) cp Resources/thinkfan-lxde.desktop /usr/share/applications/thinkfan.desktop ;;
-    *) echo "Input not valid" ;;
-esac
+cp Resources/thinkfan.desktop /usr/share/applications/thinkfan.desktop
 cp src/fancontrol $install_folder/fancontrol
 ln -s $install_folder/fancontrol /bin/fancontrol
 mkdir $install_folder/Resources
