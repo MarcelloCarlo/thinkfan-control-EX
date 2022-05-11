77b4aee929092dbaa30b36dee1fbc38edbf82646
Azure <63315512+azure-possum@users.noreply.github.com>
2022-05-11 20:35:14 -0300

    remove non usable feature

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,7 @@ install_folder=/opt/fancontrol
 rm -r $install_folder
 rm -r /bin/fancontrol
 rm -r /usr/share/applications/thinkfan.desktop
+rm -r /usr/share/polkit-1/actions/Thinkfan.policy
+#rm -r ~/.config/autostart/thinkfan.desktop
 touch /etc/modprobe.d/thinkpad_acpi.conf
 echo "options thinkpad_acpi fan_control=1" > /etc/modprobe.d/thinkpad_acpi.conf
